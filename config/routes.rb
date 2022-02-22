d133db28e4f0b0d261eb2a91ec92931bd5278339
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-02-22 12:31:46 -0300

    new and create task working

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'tasks#index', as: :tasks
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks', to: 'tasks#new', as: :new_task
+  post '/', to: 'tasks#create'
 end

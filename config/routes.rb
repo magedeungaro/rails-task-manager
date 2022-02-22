6ec217afcd06a1ef67df88eb9d3e6de0b2ded6ec
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-02-22 13:09:46 -0300

    route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index', as: :tasks
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
-  post '/', to: 'tasks#create'
+  post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
